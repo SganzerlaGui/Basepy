ac0707209056d08d3f132cc9ac4469078d07023c
SganzerlaGui <gui.sganzerla2007@gmail.com>
2026-04-22 11:21:52 -0300

    Estudando (Corrigindo a cp2 sozinho)

--- estudando_cp3.py
+++ estudando_cp3.py
@@ -1,23 +1,55 @@
-import random
-import os 
+#import random
+#import os 
+#
+#def clean():
+#    os.system('cls')
+#
+#numero = random.randint(1, 10)
+#
+#while True: 
+#
+#    adivinha = int(input("Adivinha o numero que eu estou pensando!: "))
+#
+#    if adivinha < numero:
+#        print("O numero é maior")
+#        
+#    elif adivinha == numero:
+#        print("Parabéns! Você acertou")
+#        print(f"O numero era: {numero}")
+#        break
+#        
+#    else:
+#        print("O número é menor")
 
-def clean():
-    os.system('cls')
+#nome = input("Qual o seu nome?: ")
+#print(nome)
 
-numero = random.randint(1, 10)
+semestre = 1 
 
-while True: 
+while semestre <= 2:
+   
+    cp1 = int(input("Qual a nota do seu primeiro CheckPoint?: "))
+    cp2 = int(input("Qual a nota do seu segundo CheckPoint?: "))
+    cp3 = int(input("Qual a nota do seu terceiro CheckPoint?: "))
+    sp1 = int(input("Qual foi a sua nota na Sprint1?: "))
+    sp2 = int(input("Qual foi a sua nota na Sprint2?: "))
+    gs = int(input("Qual foi a sua nota na Global Solution?: "))   
 
-    adivinha = int(input("Adivinha o numero que eu estou pensando!: "))
+    menor_cp = cp1
 
-    if adivinha < numero:
-        print("O numero é maior")
-        
-    elif adivinha == numero:
-        print("Parabéns! Você acertou")
-        print(f"O numero era: {numero}")
-        break
-        
+    if cp1 <= cp2 and cp1 <= cp3:
+        menor_cp = cp1
+    elif cp2 <= cp1 and cp2 <= cp3:
+        menor_cp = cp2
     else:
-        print("O número é menor")
+        menor_cp = cp3
+    
+    print(f"A seu menor CheckPoint foi {menor_cp}")
+
+    media = ((cp1 + cp2 + cp3 - menor_cp + sp1 + sp2) / 4) * 0.4 + gs * 0.6
+    
+    print(f"A sua média do bimestre {semestre} foi: {media:.1f}")
+    
+    semestre += 1
+
 
